d8a42f4de0e08f7315f34a90b6a5d0d431342917
max13fr <max13fr@yozo.fr>
2025-01-08 16:35:45 +0100

    fix: follow change in new version of homeassistant (TEMP_CELSIUS => UnitOfTemperature.TEMP_CELSIUS)

--- custom_components/airzonecloud/climate.py
+++ custom_components/airzonecloud/climate.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any, Dict, List, Optional
 from datetime import timedelta
-from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
+from homeassistant.const import UnitOfTemperature, ATTR_TEMPERATURE
 from homeassistant.util.unit_conversion import TemperatureConverter
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
@@ -82,7 +82,7 @@ class AirzonecloudDevice(ClimateEntity):
     @property
     def temperature_unit(self):
         """Return the unit of measurement used by the platform."""
-        return TEMP_CELSIUS
+        return UnitOfTemperature.TEMP_CELSIUS
 
     @property
     def hvac_mode(self) -> str:
@@ -149,14 +149,14 @@ class AirzonecloudDevice(ClimateEntity):
     def min_temp(self) -> float:
         """Return the minimum temperature."""
         return TemperatureConverter.convert(
-            self._device.min_temperature, TEMP_CELSIUS, self.temperature_unit
+            self._device.min_temperature, UnitOfTemperature.TEMP_CELSIUS, self.temperature_unit
         )
 
     @property
     def max_temp(self) -> float:
         """Return the maximum temperature."""
         return TemperatureConverter.convert(
-            self._device.max_temperature, TEMP_CELSIUS, self.temperature_unit
+            self._device.max_temperature, UnitOfTemperature.TEMP_CELSIUS, self.temperature_unit
         )
 
     def set_temperature(self, **kwargs) -> None:
@@ -213,7 +213,7 @@ class AirzonecloudGroup(ClimateEntity):
     @property
     def temperature_unit(self):
         """Return the unit of measurement used by the platform."""
-        return TEMP_CELSIUS
+        return UnitOfTemperature.TEMP_CELSIUS
 
     @property
     def hvac_mode(self) -> str:
